9cdba00c7e2bc1b9907ae578d478a5c1f97836da
Sige001 <72974430+Sige001@users.noreply.github.com>
2025-10-30 18:35:44 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,11 +4,10 @@ class Config:
     if os.environ.get('DATABASE_URL'):
         SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     else:
-        # For local develpement with SQLite
-        basedir = os.path.abspath(os.path.dirname(__file__))
-        SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.sql') 
+        # For local development with MySQL (XAMPP)
+        SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:@localhost/eventify_db'
         
-    SQLALCHEMY_TRACK_MODIFICATION = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     
     SECRET_KEY = os.environ.get("SECRET_KEY") or "my_SeCrEt_keY"
     
